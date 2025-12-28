502a098f9b304ada34c2d89f611b00ea2ae46616
selim <selim-kose@live.com>
2025-12-28 15:13:00 +0100

    Add new cors origin

--- backend/src/main/java/se/selimkose/exdevsecops/controller/UserController.java
+++ backend/src/main/java/se/selimkose/exdevsecops/controller/UserController.java
@@ -10,7 +10,7 @@ import se.selimkose.exdevsecops.service.UserService;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:5000"})
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")
