2fe0e0da91eadfe53617b2ad08d006e6a838deb1
Selim Köse <107941258+selim-kose@users.noreply.github.com>
2025-12-28 15:11:23 +0100

    Add style changes and refactor code (#4)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,29 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink  } from '@angular/router';
-import { UserInput } from './components/user-input/user-input';
-import { UserDelete } from "./components/user-delete/user-delete";
-import { UserUpdate } from "./components/user-update/user-update";
-import { UserList } from "./components/user-list/user-list";
 import { Navigation } from './components/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserInput, UserDelete, UserUpdate, UserList, Navigation],
+  imports: [RouterOutlet, Navigation],
   template: `
     <main class="main">
-      <h1>Welcome to ex-devSecOps!</h1>
+      <h1>Welcome User Management!</h1>
       <app-navigation></app-navigation>
-      <div class="user-management"> 
-      </div>
     </main>
 
     <router-outlet /> 
     `,
   styles: `
-    .user-management {
-      display: flex;
-      gap: 50px;
-      margin-top: 30px;
+    .main {
+      margin-bottom: 2rem;
+      max-width: 50rem;
     }
   `,
 })
